188e3115989950587ac5d7fd401a06433f53581e
Arkadiy <czseo@yandex.ru>
2020-04-17 16:35:31 +0200

    Добавил текст в главную вьюху ветки мастер

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@ Main Page title
 
 @section('content')
 <h1>Main Page new text</h1>
+test text in master branch
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur corporis deserunt ducimus enim eos facere laudantium numquam odio, quia quisquam, quod quos rem rerum tenetur. Culpa ipsam nisi vero?</p>
 
 @endsection
